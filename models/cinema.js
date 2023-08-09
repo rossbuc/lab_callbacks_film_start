c6d23cec11642b4f482ee7e9dc64f2d3dd448ce0
Ross Buchan <rossbuchan1@googlemail.com>
2023-08-09 11:35:47 +0100

    Add find film by title method

--- models/cinema.js
+++ models/cinema.js
@@ -10,4 +10,9 @@ Cinema.prototype.getTitles = function (cinema) {
   return titles;
 };
 
+Cinema.prototype.findByTitle = function (cinema) {
+  const film = cinema.films.find((film) => film.title === 'Dunkirk')
+  return film;
+};
+
 module.exports = Cinema;
--- specs/cinema_spec.js
+++ specs/cinema_spec.js
@@ -31,8 +31,13 @@ describe('Cinema', function () {
   it('should be able to get a list of film titles', function () {
     const listOfTitles = cinema.getTitles(cinema);
     const actual = listOfTitles;
+    const expected = ['Moonlight', 'Blade Runner 2049', 'Dunkirk', 'Black Panther', 'T2 Trainspotting'];
+    assert.deepStrictEqual(actual, expected);
   });
-  it('should be able to find a film by title');
+  it('should be able to find a film by title', function () {
+    const actual = cinema.findByTitle(cinema);
+    assert.deepStrictEqual(actual, dunkirk);
+  });
   it('should be able to filter films by genre');
   it('should be able to check whether there are some films from a particular year');
   it('should be able to check whether there are no films from a particular year');
